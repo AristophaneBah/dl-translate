694db640d9f43ebd3a18325dd993044a7dc97923
Aristophane Bah <124389136+AristophaneBah@users.noreply.github.com>
2026-01-04 23:28:08 -0500

    Delete backend/test_mali_ocr.py

--- backend/test_mali_ocr.py
+++
@@ -1,7 +0,0 @@
-from app.ocr.mali_ocr import ocr_mali_french
-
-# Put a Mali license image in backend/ and name it sample_mali.jpg
-res = ocr_mali_french("sample_mali.jpg")
-
-print(res.meta)
-print(res.text[:1500])
